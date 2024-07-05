d4dfabf974553d77650bbc530993cd43b64df4f9
pglpm <luca@magnaspesmeretrix.org>
2024-07-05 11:42:38 +0200

    add R functions

---
+++ tests/testpackage_externalparallel.R
@@ -0,0 +1,71 @@
+startdir <- getwd()
+
+#### Check and change working directory if necessary
+if(basename(startdir) == 'tests'){
+  setwd(file.path('..','R'))
+  cat('\nSwitching to "R" directory\n')
+} else if(basename(startdir) == 'bayes_nonparametric_inference'){
+  setwd('R')
+  cat('\nSwitching to "R" directory\n')
+} else if(basename(startdir) != 'R'){
+  stop('Please run this script from directory "R" or "tests" or base')
+}
+
+source('bnpi.R')
+
+testdir <- file.path('..', 'tests')
+
+refdir <- file.path(testdir,
+                    'reference_seed16_packagetest-240705T064934-vrt8_dat15_smp120'
+                    )
+
+seed <- 16
+
+outputdirPrefix <- file.path(testdir,'__packagetest')
+
+ncores <- 4
+library('doParallel')
+mycluster <- makeCluster(ncores, outfile="")
+registerDoParallel(mycluster)
+
+currenttestdir <- inferpopulation(data = file.path(testdir, 'testdata.csv'),
+                        metadata = file.path(testdir, 'metatestdata.csv'),
+                        outputdir = outputdirPrefix,
+                        output = 'directory',
+                        appendtimestamp = TRUE, appendinfo = TRUE,
+                        nsamples = 120, nchains = 12,
+                        cleanup = FALSE, # parallel = 4,
+                        useOquantiles = FALSE,
+                        seed = seed)
+
+## Close connections to cores
+stopCluster(mycluster)
+
+
+#### Test whether Fdistribution output is identical
+cat('\nVerifying equality of "Fdistribution.rds" (TRUE = passed):\n')
+print(identical(
+  readRDS(file.path(currenttestdir,'Fdistribution.rds')),
+  readRDS(file.path(refdir,'Fdistribution.rds'))
+))
+
+#### Test whether MCtraces output is identical
+cat('\nVerifying equality of "MCtraces.rds" (TRUE = passed):\n')
+print(identical(
+  readRDS(file.path(currenttestdir,'MCtraces.rds')),
+  readRDS(file.path(refdir,'MCtraces.rds'))
+))
+
+#### Test whether computation log-1 is identical
+## remove lines containing diagnostic times, as they can vary
+cat('\nVerifying equality of "log-1" (TRUE = passed):\n')
+reffile <- readLines(file.path(refdir,'log-1.txt'))
+reffile <- reffile[!grepl('time', reffile, fixed=TRUE)]
+currentfile <- readLines(file.path(currenttestdir,'log-1.log'))
+currentfile <- currentfile[!grepl('time', currentfile, fixed=TRUE)]
+
+print(identical(currentfile, reffile))
+
+#### return to original directory, in case called with 'source'
+cat('\nSwitching to original directory\n')
+setwd(startdir)
